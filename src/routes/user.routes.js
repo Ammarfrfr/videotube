2b6098d456ccda4b5115796c692e28c51979aa92
Ammarfrfr <157887137+Ammarfrfr@users.noreply.github.com>
2026-01-26 23:11:11 +0530

    donw with the fck ass MERN

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { logOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { logOutUser, loginUser, refreshAccessToken, registerUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserWatchHistory, getUserChannelProfile } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+
 
 
 const router = Router()
@@ -25,5 +26,15 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwt, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account-details").patch(verifyJwt, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/watch-history").get(verifyJwt, getUserWatchHistory)
+router.route("/get-user-profile").get(verifyJwt, getUserChannelProfile)
 
 export default router
